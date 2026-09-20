3e2b43daef3a410f294b4891cd675a208d8dd3de
ESMA <90682542+esmaackgz@users.noreply.github.com>
2026-09-20 13:23:47 +0300

    Update timesor.py

--- timesor.py
+++ timesor.py
@@ -7,9 +7,9 @@ dizit = toku.readlines()
 print(dizit)
 
 
-gonderici_mail = 'esma.ackgz067@gmail.com'
+gonderici_mail = ''
 gonderici_sifre = ''
-alicilar = ['esma.ackgz067@gmail.com']
+alicilar = ['']
 
 
 pattern = "\d{4}[/.-]\w{2,3}[/.-]\d{2}|\d{8}|\d{1,2}[/.-]\w{1,3}[/.-]\d{4}|\w{1,4}[ ]\w{2,3}[ ]\d{4}"
